35f713d765a6af66995ac33d359ca3aafe181a81
Kemot <tomasz.budzynski.programista@gmail.com>
2021-08-09 21:58:45 +0200

    Set up FontSizePicker

--- src/main/java/pl/kemot/controller/OptionsWindowController.java
+++ src/main/java/pl/kemot/controller/OptionsWindowController.java
@@ -7,6 +7,7 @@ import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Slider;
 import pl.kemot.EmailManager;
 import pl.kemot.view.ColorTheme;
+import pl.kemot.view.FontSize;
 import pl.kemot.view.ViewFactory;
 
 import java.net.URL;
@@ -37,10 +38,23 @@ public class OptionsWindowController extends BaseController implements Initializ
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         setUpThemePicker();
+        setUpFontPicker();
+    }
+
+    private void setUpFontPicker() {
+        fontSizePicker.setMin(0);
+        fontSizePicker.setMax(FontSize.values().length - 1);
+        fontSizePicker.setValue(viewFactory.getFontSize().ordinal());
+        fontSizePicker.setMajorTickUnit(1);
+        fontSizePicker.setMinorTickCount(0);
+        fontSizePicker.setBlockIncrement(1);
+        fontSizePicker.setSnapToTicks(true);
+        fontSizePicker.setShowTickMarks(true);
+        fontSizePicker.setShowTickLabels(true);
     }
 
     private void setUpThemePicker() {
-        themePicker.setItems(FXCollections.observableArrayList(ColorTheme.values()));
+        themePicker.setItems(FXCollections.observableArrayList(ColorTheme.values())); // set items requires observable list
         themePicker.setValue(viewFactory.getColorTheme());
     }
 }
