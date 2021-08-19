de41b8b80372f2e6de8cfc99fd138747f8542901
Kemot <tomasz.budzynski.programista@gmail.com>
2021-08-19 21:27:52 +0200

    Add EmailTreeItem class

--- src/main/java/pl/kemot/EmailManager.java
+++ src/main/java/pl/kemot/EmailManager.java
@@ -2,23 +2,38 @@ package pl.kemot;
 
 import javafx.scene.control.TreeItem;
 import pl.kemot.model.EmailAccount;
+import pl.kemot.model.EmailTreeItem;
+
+import javax.mail.Folder;
+import javax.mail.MessagingException;
+import javax.mail.Store;
 
 public class EmailManager {
 
     //Folder handling
-    private TreeItem<String> foldersRoot = new TreeItem<>("");
+    private EmailTreeItem<String> foldersRoot = new EmailTreeItem<>("");
 
-    public TreeItem<String> getFoldersRoot() {
+    public EmailTreeItem<String> getFoldersRoot() {
         return foldersRoot;
     }
 
     public void addEmailAccount(EmailAccount emailAccount){
-        TreeItem<String> emailTreeItem = new TreeItem<>(emailAccount.getAdress());
+        EmailTreeItem<String> emailTreeItem = new EmailTreeItem<>(emailAccount.getAdress());
         emailTreeItem.setExpanded(true);
-            emailTreeItem.getChildren().add(new TreeItem<String>("INBOX"));
-            emailTreeItem.getChildren().add(new TreeItem<String>("Sent"));
-            emailTreeItem.getChildren().add(new TreeItem<String>("Inportant"));
-            emailTreeItem.getChildren().add(new TreeItem<String>("Spam"));
+            emailTreeItem.getChildren().add(new EmailTreeItem<String>("INBOX"));
+            emailTreeItem.getChildren().add(new EmailTreeItem<String>("Sent"));
+            emailTreeItem.getChildren().add(new EmailTreeItem<String>("Inportant"));
+            emailTreeItem.getChildren().add(new EmailTreeItem<String>("Spam"));
         foldersRoot.getChildren().add(emailTreeItem);
+    }
+
+    public void showEmailsFolders(EmailAccount emailAccount) throws MessagingException {
+        Store store = emailAccount.getStore();
+        System.out.println("Stor: " + store);
+
+        Folder[] folders = store.getDefaultFolder().list();
+        for (Folder folder : folders) {
+            System.out.println(">> " + folder.getName());
+        }
     }
 }
