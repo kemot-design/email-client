f2444e0dbcb54c59836d98d899adf75aadced2f2
Kemot <tomasz.budzynski.programista@gmail.com>
2021-08-22 22:24:03 +0200

    Add Comparable interface to SizeInteget to compare values properly

--- src/main/java/pl/kemot/model/SizeInteger.java
+++ src/main/java/pl/kemot/model/SizeInteger.java
@@ -1,6 +1,6 @@
 package pl.kemot.model;
 
-public class SizeInteger {
+public class SizeInteger implements Comparable<SizeInteger> {
 
     private int size;
 
@@ -19,6 +19,17 @@ public class SizeInteger {
             return size / 1024 + "kB";
         } else {
             return size / 1048576 + "MB";
+        }
+    }
+
+    @Override
+    public int compareTo(SizeInteger otherSizeInteger) {
+        if(size > otherSizeInteger.size){
+            return 1;
+        } else if(size < otherSizeInteger.size){
+            return -1;
+        } else {
+            return 0;
         }
     }
 }
